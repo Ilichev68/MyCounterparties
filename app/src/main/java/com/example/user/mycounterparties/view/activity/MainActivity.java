aaf004fb38726dcd6a16f9adcf417e2aa8f053cc
Sadist <Arhimagrus@mail.ru>
2017-12-07 22:34:00 +0300

    searchView with RV

--- app/src/main/java/com/example/user/mycounterparties/view/activity/MainActivity.java
+++ app/src/main/java/com/example/user/mycounterparties/view/activity/MainActivity.java
@@ -1,33 +1,91 @@
 package com.example.user.mycounterparties.view.activity;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.user.mycounterparties.R;
-import com.example.user.mycounterparties.view.fragments.LastCounterpartiesFragment;
+import com.example.user.mycounterparties.presenter.LastCounterpartiesPresenter;
+import com.example.user.mycounterparties.presenter.interfaces.ILastCounterpartiesPresenter;
+import com.example.user.mycounterparties.view.CounterpartiesItem;
+import com.example.user.mycounterparties.view.adapters.RecyclerViewAdapter;
+import com.example.user.mycounterparties.view.interfaces.ILastCunterpartiesView;
+import com.melnykov.fab.FloatingActionButton;
+
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.Listner, ILastCunterpartiesView {
 
-    private LastCounterpartiesFragment lastCounterpartiesFragment;
-    private FragmentManager manager;
-    private FragmentTransaction transaction;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
+    private FloatingActionButton fab;
+    private TextView textViewIfRvEmpy;
+    private ILastCounterpartiesPresenter presenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lastCounterpartiesFragment = new LastCounterpartiesFragment();
+        initialize(this);
 
-        manager = getSupportFragmentManager();
-        transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, lastCounterpartiesFragment).commit();
+
+        recyclerView = findViewById(R.id.rv);
+        fab = findViewById(R.id.fab);
+        textViewIfRvEmpy = findViewById(R.id.text_if_rv_empty);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.startSearchActivity(MainActivity.this);
+            }
+        });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        presenter.downloadCounterpartiesFromCache();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter.downloadCounterpartiesFromCache();
+    }
+
+
+    @Override
+    public void onClick(String nameAndAddress) {
+        presenter.startCounterpatiyActivity(this, nameAndAddress);
+    }
+
+    @Override
+    public void onCheckedChanged(boolean isChecked, String counterpartiesName) {
+        presenter.setCheckedChanged(isChecked, counterpartiesName);
+
+    }
+
+    @Override
+    public void showAllLastCounterparties(List<CounterpartiesItem> counterpartiesItems) {
+        if (counterpartiesItems.size() == 0) textViewIfRvEmpy.setVisibility(View.VISIBLE);
+        else textViewIfRvEmpy.setVisibility(View.INVISIBLE);
+
+        adapter = new RecyclerViewAdapter(counterpartiesItems);
+        adapter.setListner(this);
+        recyclerView.setAdapter(adapter);
+        fab.attachToRecyclerView(recyclerView);
+
+    }
+
+    private void initialize(ILastCunterpartiesView iLastCunterpartiesView) {
+        presenter = new LastCounterpartiesPresenter(iLastCunterpartiesView);
     }
 
     @Override
--- app/src/main/java/com/example/user/mycounterparties/view/activity/SearchActivity.java
+++ app/src/main/java/com/example/user/mycounterparties/view/activity/SearchActivity.java
@@ -2,19 +2,31 @@ package com.example.user.mycounterparties.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.example.user.mycounterparties.R;
-import com.example.user.mycounterparties.view.fragments.SearchFragment;
+import com.example.user.mycounterparties.presenter.SearchPresenter;
+import com.example.user.mycounterparties.presenter.interfaces.ISearchPresenter;
+import com.example.user.mycounterparties.view.adapters.DaDataArrayAdapter;
+import com.example.user.mycounterparties.view.interfaces.ISearchView;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private SearchFragment searchFragment;
-    private FragmentManager manager;
-    private FragmentTransaction transaction;
+public class SearchActivity extends AppCompatActivity implements TextWatcher, ISearchView {
+
+    private static final List<String> EMPTY = new ArrayList<>();
+    private DaDataArrayAdapter<String> adapter;
+    private AutoCompleteTextView textView;
+    private Toast toast;
+    private ISearchPresenter presenter;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, SearchActivity.class);
@@ -26,10 +38,63 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        searchFragment = new SearchFragment();
+        textView = findViewById(R.id.autocompletetextview_activitymain);
+        adapter = new DaDataArrayAdapter<>(this, android.R.layout.simple_list_item_1, EMPTY);
 
-        manager = getSupportFragmentManager();
-        transaction = manager.beginTransaction();
-        transaction.replace(R.id.container_for_search, searchFragment).commit();
+        initialize(this);
+
+        textView.setAdapter(adapter);
+
+        textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                CounterpartiyActivity.start(SearchActivity.this, adapter.getItem(i));
+                textView.setText("");
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        textView.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(final Editable s) {
+        presenter.sendQueryToDaData(s.toString());
+    }
+
+
+    @Override
+    public void showError(String error) {
+        if (toast != null)
+            toast.cancel();
+
+        toast = Toast.makeText(this, error, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public synchronized void showSuggestions(List<String> suggestions) {
+        adapter.clear();
+
+        adapter.addAll(suggestions);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initialize(ISearchView iSearchView) {
+        presenter = new SearchPresenter(iSearchView);
     }
 }
